4747c7ea69d42961c597abf4299bee4871331d9b
atsunori nishiyama <nishiyama1980@icloud.com>
2020-05-20 22:48:23 +0900

    isLikedBy メソッドを作成

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -21,6 +22,27 @@ class Article extends Model
         //thisは、Articleクラスのインスタンス自身を指す
         // $this->メソッド名()とすることで、インスタンスが持つメソッドが実行
         // $this->プロパティ名とすることで、インスタンスが持つプロパティを参照
+    }
+
+    public function likes(): BelongsToMany
+    {
+        //belongsToManyメソッドの第一引数には関係するモデルのモデル名を
+        //第二引数には中間テーブルのテーブル名を渡し
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+        //withTimestamps:likesテーブルには、created_at、updated_atカラムが存在
+    }
+
+    // [?]を付けると、その引数がnullであることも許容
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            //三項演算子を用いて$userがnullかどうかによって処理を振り分け
+            //この記事をいいねしたユーザーの中に、引数として渡された$userがいるかどうかを調べる
+            //countメソッドは、コレクションの要素数を数えて、数値を返し
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
+            //$userがnullであれば、falseを返し
+            //型キャスト:(bool)と記述することで変数を論理値、つまりtrueかfalseに変換
     }
 
 }
--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -5,10 +5,27 @@
       class="btn m-0 p-1 shadow-none"
     >
       <i class="fas fa-heart mr-1"
+       :class="{'red-text':this.isLikedBy}" 
       />
     </button>
     10
   </div>
 </template>
 <script>
+  export default {
+    props: {
+      //Blade側で、initial-is-liked-byに渡した値は、プロパティinitialIsLikedByに渡され
+      initialIsLikedBy: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    data() {
+      return {
+        //プロパティinitialIsLikedByの値をそのままデータisLikedByにセット
+        //データisLikedByを別途定義して、こちらを利用する
+        isLikedBy: this.initialIsLikedBy,
+      }
+    },
+  }
 </script>
