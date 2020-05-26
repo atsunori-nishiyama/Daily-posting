a6aa228042caf12154c76ceb0a26fed1f817875b
atsunori nishiyama <nishiyama1980@icloud.com>
2020-05-27 08:56:29 +0900

    タグ機能

--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -5,7 +5,7 @@
       class="btn m-0 p-1 shadow-none"
     >
       <i class="fas fa-heart mr-1"
-       :class="{'red-text':this.isLikedBy}" 
+       :class="{'red-text':this.isLikedBy, 'animated heartBeat fast' :this.gotToLike}" 
        @click="clickLike"
       />
     </button>
@@ -38,6 +38,7 @@
         //データisLikedByを別途定義して、こちらを利用する※countLikes:も同様
         isLikedBy: this.initialIsLikedBy,
         countLikes: this.initialCountLikes,
+        gotToLike: false,
       }
     },
     methods: {
@@ -57,12 +58,14 @@
 
         this.isLikedBy = true
         this.countLikes = responce.data.countLikes //いいね数を取得、いいね数の表示が変更
+        this.gotToLike = true
       },
       async unlike() {
         const responce = await axios.delete(this.endpoint)
 
         this.isLikedBy = false
         this.countLikes = responce.data.countLikes
+        this.gotToLike = false
       },
     },
   }
