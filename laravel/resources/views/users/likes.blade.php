02bd209069d599ce64d9861710632119f0592ad7
atsunori nishiyama <nishiyama1980@icloud.com>
2020-06-03 21:30:36 +0900

    イイネ記事表示機能と追加 Bladeの共通化

---
+++ laravel/resources/views/users/likes.blade.php
@@ -0,0 +1,28 @@
+@extends('app')
+
+@section('title', $user->name . 'のイイネした記事')
+
+@section('content')
+ @include('nav')
+ <div class="container">
+  @include('users.user')
+  @include('users.tabs', ['hasArticles' => false, 'hasLikes' => true])
+  <!-- <ul class="nav nav-tabs nav-justified mt-3">
+    <li class="nav-item">
+      <a class="nav-link text-muted"
+        href="{{ route('users.show', ['name' => $user->name]) }}">
+        記事
+        </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-muted active"
+        href="{{ route('users.likes', ['name' => $user->name]) }}">
+        イイネ
+        </a>
+    </li>
+  </ul> -->
+  @foreach($articles as $article)
+   @include('articles.card')
+  @endforeach
+ </div>
+@endsection
--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -5,40 +5,9 @@
 @section('content')
   @include('nav')
   <div class="container">
-    <div class="card mt-3">
-      <div class="card-body">
-        <div class="d-flex flex-row">
-          <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-drak">
-            <i class="far fa-smile-beam fa-3x mr-1"></i>
-          </a>
-          @if( Auth::id() !== $user->id )
-            <follow-button
-              class="ml-auto"
-              :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
-              :authorized='@json(Auth::check())'
-              endpoint="{{ route('users.follow', ['name' => $user->name]) }}"
-            >
-            </follow-button>
-          @endif
-        </div>
-        <h2 class="h5 card-title m-0">
-          <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-            {{ $user->name }}
-          </a>
-        </h2>
-      </div>
-      <div class="card-body">
-        <div class="card-text">
-          <a href="" class="text-muted">
-            {{ $user->count_followings }} フォロー
-          </a>
-          <a href="" class="text-muted">
-            {{ $user->count_followers }} フォロワー
-          </a>
-        </div>
-      </div>
-    </div>
-    <ul class="nav nav-tabs nav-justified mt-3">
+    @include('users.user')
+    @include('users.tabs', ['hasArticles' => true, 'hasLikes' => false])
+    <!-- <ul class="nav nav-tabs nav-justified mt-3">
       <li class="nav-item">
         <a class="nav-link text-muted active"
           href="{{ route('users.show', ['name' => $user->name]) }}">
@@ -47,11 +16,11 @@
       </li>
       <li class="nav-item">
         <a class="nav-link text-muted"
-          href="">
-          いいね  
+          href="{{ route('users.likes', ['name' => $user->name]) }}">
+          イイネ  
         </a>
       </li>
-    </ul>
+    </ul> -->
     @foreach($articles as $article)
       @include('articles.card')
     @endforeach
