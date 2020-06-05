c9982aa5e23a87515d0f1b6996ae7c0a85c2c318
atsunori nishiyama <nishiyama1980@icloud.com>
2020-06-06 08:07:45 +0900

    フォロー中、フォロワー一覧表示機能を追加

---
+++ laravel/resources/views/users/person.blade.php
@@ -0,0 +1,21 @@
+<div class="card mt-3">
+  <div class="card-body">
+    <div class="d-flex flex-row">
+      <a href="{{ route('users.show', ['name' => $person->name]) }}" class="text-dark">
+      <i class="far fa-smile-beam fa-3x mr-1"></i>
+      </a>
+      @if( Auth::id() !== $person->id )
+        <follow-button
+          class="ml-auto"
+          :initial-is-follwed-by='@json($person->isFollowedBy(Auth::user()))'
+          :authorized='@json(Auth::check())'
+          endpoint="{{ route('users.follow', ['name' => $person->name]) }}"
+        >
+        </follow-button>
+      @endif
+    </div>
+    <h2 class="h5 card-title m-0">
+      <a href="{{ route('users.show', ['name' => $person->name]) }}" class="text-dark">{{ $person->name }}</a>
+    </h2>
+  </div>
+</div>
