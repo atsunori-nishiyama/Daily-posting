7e4dbe91ee54279a604b018dc42a8b3715a1654c
atsunori nishiyama <nishiyama1980@icloud.com>
2020-06-06 09:15:14 +0900

    Google認証機能を追加

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -22,6 +22,9 @@
 // use Illuminate\Routing\Route;
 
 Auth::routes();
+Route::prefix('login')->name('login.')->group(function () {
+  Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+});
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 //'auth':リクエストをコントローラーで処理する前にユーザーがログイン済みであるかどうかをチェック
